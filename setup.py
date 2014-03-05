288663d82d97fc63b8ddde13a3eb388fc425cfa4
Jeff Hann <jeffhann@gmail.com>
2014-03-05 10:05:46 -0400

    adding screenshots, setup.py script, and color switching support

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+	name='RdioCommander',
+	version='0.1.0',
+	author='Jeffrey Hann',
+	author_email='jeffhann@gmail.com',
+	scripts=['bin/rdio'],
+	url='http://obihann.github.io/RdioCommander/',
+	license='LICENSE',
+	description='A Python and AppleScript tool for monitoring the OSX Rdio application.',
+    requires=['urwid'],
+    classifiers=[
+        'Environment :: Console',
+        'Operating System :: MacOS :: MacOS X',
+    ],
+)
