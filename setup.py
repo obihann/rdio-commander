b4f4e7f2fd487cdbeb99ed5ea9f4b67da2252843
Jeff Hann <jeffhann@gmail.com>
2014-03-06 16:41:28 -0400

    fixing requirments

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
-	name='RdioCommander',
-	version='1.2.0',
-	author='Jeffrey Hann',
-	author_email='jeffhann@gmail.com',
-	scripts=['bin/rdio'],
-	url='http://obihann.github.io/RdioCommander/',
-	license='LICENSE',
-	description='A Python and AppleScript tool for monitoring the OSX Rdio application.',
-    requires=['urwid'],
-    classifiers=[
-        'Environment :: Console',
-        'Operating System :: MacOS :: MacOS X',
-    ],
-)
+        name='RdioCommander',
+        version='1.2.0',
+        author='Jeffrey Hann',
+        author_email='jeffhann@gmail.com',
+        scripts=['bin/rdio'],
+        url='http://obihann.github.io/RdioCommander/',
+        license='LICENSE',
+        description='A Python and AppleScript tool for monitoring the OSX Rdio application.',
+        install_requires=['urwid'],
+        classifiers=[
+            'Environment :: Console',
+            'Operating System :: MacOS :: MacOS X',
+            ],
+        )
