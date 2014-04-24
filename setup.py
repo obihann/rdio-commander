916e10292270371a4e5b96b4b430c4bb6bce804b
Jeff Hann <jeffhann@gmail.com>
2014-04-24 11:38:27 -0300

    adding travis ci for testing and updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
         name='RdioCommander',
-        version='1.2.0',
+        version='1.2.1',
         author='Jeffrey Hann',
         author_email='jeffhann@gmail.com',
         scripts=['bin/rdio'],
